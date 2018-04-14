13f1d941e8ba6ecb6d9d1313167659511796ef82
徐誉诚 <scottxu@SCOTTXU-MBP>
2018-04-14 14:59:50 +0800

    修改版本信息

--- Scottxu.WebServiceDataProvider/Properties/AssemblyInfo.cs
+++ Scottxu.WebServiceDataProvider/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2018.4.*")]
-[assembly: AssemblyFileVersion("2018.4.0.0")]
+[assembly: AssemblyFileVersion("2018.4.1.0")]
